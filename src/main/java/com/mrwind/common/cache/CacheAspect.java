7060aa19f1625e36d2e90f4fb77ce2517f07e75c
zhoujie <zhoujie@123feng.com>
2017-01-03 16:21:14 +0800

    修改了框架

--- src/main/java/com/mrwind/common/cache/CacheAspect.java
+++ src/main/java/com/mrwind/common/cache/CacheAspect.java
@@ -1,12 +1,17 @@
 package com.mrwind.common.cache;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -15,100 +20,188 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import com.mrwind.common.annotation.CacheEvict;
+import com.mrwind.common.annotation.CacheKey;
 import com.mrwind.common.annotation.Cacheable;
+import com.mrwind.common.annotation.Cacheable.KeyMode;
+import com.mrwind.common.annotation.CacheableLong;
 import com.mrwind.common.util.SerializableUtil;
 
-@Component
 @Aspect
+@Component
 public class CacheAspect {
-	
-    @Resource RedisCache redisCache;
-    
-    /**          
-    * 定义缓存逻辑                    
-    */
-    @Around("@annotation(com.mrwind.common.annotation.Cacheable)")
-    public Object cache(ProceedingJoinPoint pjp ) {
-        Object result=null;
-        
-        Method method=getMethod(pjp);
-        Cacheable cacheable=method.getAnnotation(Cacheable.class);
-        
-        String fieldKey =parseKey(cacheable.fieldKey(),method,pjp.getArgs());
-        
-        //使用redis 的hash进行存取，易于管理
-        result= redisCache.hget(cacheable.key().getBytes(), fieldKey.getBytes());
-        if(result==null){
-            try {
-                result=pjp.proceed();
-                Assert.notNull(fieldKey);
-                redisCache.hset(cacheable.key().getBytes(),fieldKey.getBytes(),SerializableUtil.serialize(result));
-            } catch (Throwable e) {
-                e.printStackTrace();
-            }
-        }
-        return result;
-    }
-    /**          * 定义清除缓存逻辑          */
-    @Around(value="@annotation(com.mrwind.common.annotation.CacheEvict)")
-    public Object evict(ProceedingJoinPoint pjp ){
-        Method method=getMethod(pjp);
-        CacheEvict cacheEvict=method.getAnnotation(CacheEvict.class);
-        
-        String fieldKey =parseKey(cacheEvict.fieldKey(),method,pjp.getArgs());
-        
-        //使用redis 的hash进行存取，易于管理
-        return redisCache.hdel(cacheEvict.key().getBytes(), fieldKey.getBytes());
-    }
-    
-    /**
-     *  获取被拦截方法对象
-     *  
-     *  MethodSignature.getMethod() 获取的是顶层接口或者父类的方法对象
-     *    而缓存的注解在实现类的方法上
-     *  所以应该使用反射获取当前对象的方法对象
-     */
-    @SuppressWarnings("rawtypes")
-	public Method getMethod(ProceedingJoinPoint pjp){
-        //获取参数的类型
-        Object [] args=pjp.getArgs();
-        Class [] argTypes=new Class[pjp.getArgs().length];
-        for(int i=0;i<args.length;i++){
-            argTypes[i]=args[i].getClass();
-        }
-        Method method=null;
-        try {
-            method=pjp.getTarget().getClass().getMethod(pjp.getSignature().getName(),argTypes);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-        return method;
-        
-    }
-    /**
-     *    获取缓存的key 
-     *    key 定义在注解上，支持SPEL表达式
-     * @param pjp
-     * @return
-     */
-    private String parseKey(String key,Method method,Object [] args){
-        
-        
-        //获取被拦截方法参数名列表(使用Spring支持类库)
-        LocalVariableTableParameterNameDiscoverer u =   
-            new LocalVariableTableParameterNameDiscoverer();  
-        String [] paraNameArr=u.getParameterNames(method);
-        
-        //使用SPEL进行key的解析
-        ExpressionParser parser = new SpelExpressionParser(); 
-        //SPEL上下文
-        StandardEvaluationContext context = new StandardEvaluationContext();
-        //把方法参数放入SPEL上下文中
-        for(int i=0;i<paraNameArr.length;i++){
-            context.setVariable(paraNameArr[i], args[i]);
-        }
-        return parser.parseExpression(key).getValue(context,String.class);
-    }
+
+	@Resource
+	RedisCache redisCache;
+	private static Log log = LogFactory.getLog(CacheAspect.class);
+
+	@Pointcut("@annotation(com.mrwind.common.annotation.Cacheable)")
+	public void cacheFilter() {
+	}
+
+	@Pointcut("@annotation(com.mrwind.common.annotation.CacheableLong)")
+	public void cacheLongFilter() {
+	}
+
+	/**
+	 * 定义缓存逻辑
+	 */
+	@Around("cacheFilter()")
+	public Object cache(ProceedingJoinPoint pjp) {
+		Object result = null;
+		Method method = getMethod(pjp);
+		Cacheable cacheable = method.getAnnotation(Cacheable.class);
+
+		String fieldKey = parseKey(cacheable.fieldKey(), method, pjp.getArgs());
+		// 使用redis 的hash进行存取，易于管理
+		log.info("feeldKey:" + fieldKey + "    key:" + cacheable.key() + " this is redis get");
+		result = redisCache.hget(cacheable.key(), fieldKey);
+		if (result == null) {
+			try {
+				log.info("feeldKey:" + fieldKey + "    key:" + cacheable.key() + " this go to db");
+				int expireTime = cacheable.expireTime();
+				result = pjp.proceed();
+				Assert.notNull(fieldKey);
+				redisCache.hset(cacheable.key().getBytes(), fieldKey.getBytes(), SerializableUtil.serialize(result));
+				redisCache.expire(cacheable.key(), expireTime);
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 定义缓存逻辑
+	 */
+	@Around("cacheLongFilter()")
+	public Long cacheBase(ProceedingJoinPoint pjp) {
+		String result = null;
+		Method method = getMethod(pjp);
+		CacheableLong cacheable = method.getAnnotation(CacheableLong.class);
+
+		String fieldKey = parseKey(cacheable.fieldKey(), method, pjp.getArgs());
+		// 使用redis 的hash进行存取，易于管理
+		log.info("feeldKey:" + fieldKey + "    key:" + cacheable.key() + " this is redis get");
+		result = redisCache.hgetString(cacheable.key(), fieldKey);
+		if (result == null || Long.valueOf(result) < 0) {
+			try {
+				log.info("feeldKey:" + fieldKey + "    key:" + cacheable.key() + " this go to db");
+				int expireTime = cacheable.expireTime();
+				result = pjp.proceed().toString();
+				Assert.notNull(fieldKey);
+				redisCache.hset(cacheable.key().getBytes(), fieldKey.getBytes(), result.getBytes());
+				redisCache.expire(cacheable.key(), expireTime);
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
+		return Long.valueOf(result);
+	}
+
+	/** * 定义清除缓存逻辑 */
+	@Around(value = "@annotation(com.mrwind.common.annotation.CacheEvict)")
+	public Object evict(ProceedingJoinPoint pjp) {
+		Method method = getMethod(pjp);
+		CacheEvict cacheEvict = method.getAnnotation(CacheEvict.class);
+
+		String fieldKey = parseKey(cacheEvict.fieldKey(), method, pjp.getArgs());
+
+		// 使用redis 的hash进行存取，易于管理
+		return redisCache.hdel(cacheEvict.key().getBytes(), fieldKey.getBytes());
+	}
+
+	/**
+	 * 获取被拦截方法对象
+	 * 
+	 * MethodSignature.getMethod() 获取的是顶层接口或者父类的方法对象 而缓存的注解在实现类的方法上
+	 * 所以应该使用反射获取当前对象的方法对象
+	 */
+	@SuppressWarnings("rawtypes")
+	public Method getMethod(ProceedingJoinPoint pjp) {
+		// 获取参数的类型
+		Object[] args = pjp.getArgs();
+		Class[] argTypes = new Class[pjp.getArgs().length];
+		for (int i = 0; i < args.length; i++) {
+			argTypes[i] = args[i].getClass();
+		}
+		Method method = null;
+		try {
+			method = pjp.getTarget().getClass().getMethod(pjp.getSignature().getName(), argTypes);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		return method;
+
+	}
+
+	/**
+	 * 获取缓存的key值
+	 * 
+	 * @param pjp
+	 * @param cache
+	 * @return
+	 */
+	@SuppressWarnings("unused")
+	private String getCacheKey(ProceedingJoinPoint pjp, Cacheable cache) {
+
+		StringBuilder buf = new StringBuilder();
+		buf.append(pjp.getSignature().getDeclaringTypeName()).append(".").append(pjp.getSignature().getName());
+		if (cache.key().length() > 0) {
+			buf.append(".").append(cache.key());
+		}
+
+		Object[] args = pjp.getArgs();
+		if (cache.keyMode() == KeyMode.DEFAULT) {
+			Annotation[][] pas = ((MethodSignature) pjp.getSignature()).getMethod().getParameterAnnotations();
+			for (int i = 0; i < pas.length; i++) {
+				for (Annotation an : pas[i]) {
+					if (an instanceof CacheKey) {
+						buf.append(".").append(args[i].toString());
+						break;
+					}
+				}
+			}
+		} else if (cache.keyMode() == KeyMode.BASIC) {
+			for (Object arg : args) {
+				if (arg instanceof String) {
+					buf.append(".").append(arg);
+				} else if (arg instanceof Integer || arg instanceof Long || arg instanceof Short) {
+					buf.append(".").append(arg.toString());
+				} else if (arg instanceof Boolean) {
+					buf.append(".").append(arg.toString());
+				}
+			}
+		} else if (cache.keyMode() == KeyMode.ALL) {
+			for (Object arg : args) {
+				buf.append(".").append(arg.toString());
+			}
+		}
+
+		return buf.toString();
+	}
+
+	/**
+	 * 获取缓存的key key 定义在注解上，支持SPEL表达式
+	 * 
+	 * @param pjp
+	 * @return
+	 */
+	private String parseKey(String key, Method method, Object[] args) {
+
+		// 获取被拦截方法参数名列表(使用Spring支持类库)
+		LocalVariableTableParameterNameDiscoverer u = new LocalVariableTableParameterNameDiscoverer();
+		String[] paraNameArr = u.getParameterNames(method);
+
+		// 使用SPEL进行key的解析
+		ExpressionParser parser = new SpelExpressionParser();
+		// SPEL上下文
+		StandardEvaluationContext context = new StandardEvaluationContext();
+		// 把方法参数放入SPEL上下文中
+		for (int i = 0; i < paraNameArr.length; i++) {
+			context.setVariable(paraNameArr[i], args[i]);
+		}
+		return parser.parseExpression(key).getValue(context, String.class);
+	}
 }
--- src/main/java/com/mrwind/common/cache/RedisCache.java
+++ src/main/java/com/mrwind/common/cache/RedisCache.java
@@ -19,322 +19,604 @@ import redis.clients.util.SafeEncoder;
 @Component
 public class RedisCache {
 	private static final String RESPONSE_OK = "OK";
-	
-	private Jedis redis = null;
-	@Resource JedisPool jedisPool;
+
+	@Resource
+	JedisPool redisPool;
 
 	/**
 	 * 对key进行模糊查询
+	 * 
 	 * @Description: TODO
-	 * @param @param key
-	 * @param @return   
-	 * @return Set<String>  
-	 * @throws
-	 * @author ZJ
+	 * @param @param
+	 *            key
+	 * @param @return
+	 * @return Set<String>
+	 * @throws @author
+	 *             ZJ
 	 * @date 2016-3-29
 	 */
-	public Set<String> getKeys(String key){
-		return redis.keys(key);
+	public Set<String> getKeys(String key) {
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.keys(key);
+		} finally {
+			redis.close();
+		}
 	}
-	
+
 	public Object set(String key, int expire, String value) {
-		redis =jedisPool.getResource(); 
-		String msg = redis.set(SafeEncoder.encode(key), SafeEncoder.encode(value));
-		if(msg.equals(RESPONSE_OK)) {
-			return redis.expire(SafeEncoder.encode(key), expire);
-		} else {
-			return msg;
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			String msg = redis.set(SafeEncoder.encode(key), SafeEncoder.encode(value));
+			if (msg.equals(RESPONSE_OK)) {
+				return redis.expire(SafeEncoder.encode(key), expire);
+			} else {
+				return msg;
+			}
+		} finally {
+			redis.close();
 		}
+
 	}
-	
+
 	public Long getPK(String tableName, int interval) {
-		long pk = this.incr("PK"+tableName, interval);
-		if(pk<10000000){
-			this.set("PK"+tableName, Integer.MAX_VALUE, 10000000+"");
-			pk=10000000;
+		long pk = this.incr("PK" + tableName, interval);
+		if (pk < 10000000) {
+			this.set("PK" + tableName, Integer.MAX_VALUE, 10000000 + "");
+			pk = 10000000;
 		}
 		return pk;
 	}
-	
+
 	public Long getTestPK(String tableName, int interval) {
-		long pk = this.incr("PK"+tableName, interval);
-		if(pk<900000000){
-			this.set("PK"+tableName, Integer.MAX_VALUE, 900000000+"");
-			pk=900000000;
+		long pk = this.incr("PK" + tableName, interval);
+		if (pk < 900000000) {
+			this.set("PK" + tableName, Integer.MAX_VALUE, 900000000 + "");
+			pk = 900000000;
 		}
 		return pk;
 	}
-	
-	
+
 	public Object set(String key, int expire, byte[] value) {
-		redis =jedisPool.getResource(); 
-		String msg = redis.set(SafeEncoder.encode(key), value);
-		if(msg.equals(RESPONSE_OK)) {
-			return redis.expire(SafeEncoder.encode(key), expire);
-		} else {
-			return msg;
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			String msg = redis.set(SafeEncoder.encode(key), value);
+			if (msg.equals(RESPONSE_OK)) {
+				return redis.expire(SafeEncoder.encode(key), expire);
+			} else {
+				return msg;
+			}
+		} finally {
+			redis.close();
 		}
 	}
-	
+
 	public Object append(String key, String value) {
-		redis =jedisPool.getResource(); 
-		return redis.append(SafeEncoder.encode(key), SafeEncoder.encode(value));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.append(SafeEncoder.encode(key), SafeEncoder.encode(value));
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Object append(String key, byte[] value) {
-		redis =jedisPool.getResource(); 
-		return redis.append(SafeEncoder.encode(key), value);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.append(SafeEncoder.encode(key), value);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public long incr(String key, long by) {
-		redis =jedisPool.getResource(); 
-		return redis.incrBy(SafeEncoder.encode(key), by);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.incrBy(SafeEncoder.encode(key), by);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public long decr(String key, long by) {
-		redis =jedisPool.getResource(); 
-		return redis.decrBy(SafeEncoder.encode(key), by);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.decrBy(SafeEncoder.encode(key), by);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public String getString(String key) {
-		redis =jedisPool.getResource(); 
-		byte[] result = redis.get(SafeEncoder.encode(key));
-		if (null != result) {
-			return SafeEncoder.encode(result);
-		} else {
-			return null;
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			byte[] result = redis.get(SafeEncoder.encode(key));
+			if (null != result) {
+				return SafeEncoder.encode(result);
+			} else {
+				return null;
+			}
+		} finally {
+			redis.close();
 		}
 	}
 
 	public byte[] getBinary(String key) {
-		redis =jedisPool.getResource(); 
-		return redis.get(SafeEncoder.encode(key));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.get(SafeEncoder.encode(key));
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Object delete(String key) {
-		redis =jedisPool.getResource(); 
-		return redis.del(SafeEncoder.encode(key));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.del(SafeEncoder.encode(key));
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long expire(String key, int seconds) {
-		redis =jedisPool.getResource(); 
-		return redis.expire(SafeEncoder.encode(key), seconds);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.expire(SafeEncoder.encode(key), seconds);
+		} finally {
+			redis.close();
+		}
 	}
-	
+
 	public byte[] substr(byte[] key, int start, int end) {
-		redis =jedisPool.getResource(); 
-		return redis.substr(key, start, end);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.substr(key, start, end);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long rpush(byte[] key, byte[]... args) {
-		redis =jedisPool.getResource(); 
-		return redis.rpush(key, args);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.rpush(key, args);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long lpush(byte[] key, byte[]... args) {
-		redis =jedisPool.getResource(); 
-		return redis.lpush(key, args);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.lpush(key, args);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public byte[] lpop(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.lpop(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.lpop(key);
+		} finally {
+			redis.close();
+		}
 	}
 
-	public byte[] rpop(byte[] key) {	
-		redis =jedisPool.getResource(); 
-		return redis.rpop(key);
+	public byte[] rpop(byte[] key) {
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.rpop(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public String ltrim(byte[] key, long start, long end) {
-		redis =jedisPool.getResource(); 
-		return redis.ltrim(key, start, end);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.ltrim(key, start, end);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public String lset(byte[] key, long index, byte[] value) {
-		redis =jedisPool.getResource(); 
-		return redis.lset(key, index, value);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.lset(key, index, value);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long llen(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.llen(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.llen(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public byte[] lindex(byte[] key, long index) {
-		redis =jedisPool.getResource(); 
-		return redis.lindex(key, index);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.lindex(key, index);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public List<byte[]> lrange(byte[] key, long start, long end) {
-		redis =jedisPool.getResource(); 
-		return redis.lrange(key, start, end);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.lrange(key, start, end);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long sadd(byte[] key, byte[]... member) {
-		redis =jedisPool.getResource(); 
-		return redis.sadd(key, member);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.sadd(key, member);
+		} finally {
+			redis.close();
+		}
 	}
-	
+
 	public Long sadd(String key, String members) {
-		redis =jedisPool.getResource(); 
-		return redis.sadd(key, members);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.sadd(key, members);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long srem(byte[] key, byte[]... member) {
-		redis =jedisPool.getResource(); 
-		return redis.srem(key, member);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.srem(key, member);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public byte[] spop(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.spop(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.spop(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long scard(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.scard(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.scard(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Boolean sismember(byte[] key, byte[] member) {
-		redis =jedisPool.getResource(); 
-		return redis.sismember(key, member);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.sismember(key, member);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public List<byte[]> srandmember(byte[] key, int count) {
-		redis =jedisPool.getResource(); 
-		return redis.srandmember(key, count);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.srandmember(key, count);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Set<byte[]> smembers(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.smembers(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.smembers(key);
+		} finally {
+			redis.close();
+		}
 	}
-	
+
 	public Set<String> smembers(String key) {
-		redis =jedisPool.getResource(); 
-		return redis.smembers(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.smembers(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long hset(byte[] key, byte[] field, byte[] value) {
-		redis =jedisPool.getResource(); 
-		return redis.hset(key, field, value);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hset(key, field, value);
+		} finally {
+			redis.close();
+		}
 	}
-	
+
 	public Long hset(String key, String field, Object value) {
-		redis =jedisPool.getResource(); 
-		return redis.hset(key.getBytes(), field.getBytes(), SerializableUtil.serialize(value));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hset(key.getBytes(), field.getBytes(), SerializableUtil.serialize(value));
+		} finally {
+			redis.close();
+		}
 	}
-	
-	
 
 	public String hmset(byte[] key, Map<byte[], byte[]> hash) {
-		redis =jedisPool.getResource(); 
-		return redis.hmset(key, hash);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hmset(key, hash);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long hincrBy(byte[] key, byte[] field, long value) {
-		redis =jedisPool.getResource(); 
-		return redis.hincrBy(key, field, value);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hincrBy(key, field, value);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			redis.close();
+		}
+		return 0l;
 	}
 
 	public Long hdel(byte[] key, byte[]... field) {
-		redis =jedisPool.getResource(); 
-		return redis.hdel(key, field);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hdel(key, field);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Boolean hexists(byte[] key, byte[] field) {
-		redis =jedisPool.getResource(); 
-		return redis.hexists(key, field);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hexists(key, field);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Long hlen(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.hlen(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hlen(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Object hget(byte[] key, byte[] field) {
-		redis =jedisPool.getResource(); 
-		return SerializableUtil.deserialize(redis.hget(key, field));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			byte[] hget = redis.hget(key, field);
+			if (hget == null)
+				return null;
+			return SerializableUtil.deserialize(hget);
+		} finally {
+			redis.close();
+		}
 	}
-	
+
 	public Object hget(String key, String field) {
-		redis =jedisPool.getResource(); 
-		return SerializableUtil.deserialize(redis.hget(key.getBytes(), field.getBytes()));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			byte[] hget = redis.hget(key.getBytes(), field.getBytes());
+			if (hget == null || hget.length == 0)
+				return null;
+			return SerializableUtil.deserialize(hget);
+		} finally {
+			redis.close();
+		}
 	}
 	
+	public String hgetString(String key, String field) {
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			byte[] hget = redis.hget(key.getBytes(), field.getBytes());
+			if (hget == null || hget.length == 0)
+				return null;
+			return new String(hget);
+		} finally {
+			redis.close();
+		}
+	}
+
 	/**
-	 *HM 是hashmap的意思 获取hashmap结构的数据
+	 * HM 是hashmap的意思 获取hashmap结构的数据
+	 * 
 	 * @Description: TODO
-	 * @param @param key
-	 * @param @param fields
-	 * @param @return   
-	 * @return List<byte[]>  
-	 * @throws
-	 * @author ZJ
+	 * @param @param
+	 *            key
+	 * @param @param
+	 *            fields
+	 * @param @return
+	 * @return List<byte[]>
+	 * @throws @author
+	 *             ZJ
 	 * @date 2016-7-1
 	 */
 	public List<byte[]> hmget(byte[] key, byte[]... fields) {
-		redis =jedisPool.getResource(); 
-		return redis.hmget(key, fields);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hmget(key, fields);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Set<byte[]> hkeys(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.hkeys(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hkeys(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Collection<byte[]> hvals(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.hvals(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hvals(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Map<byte[], byte[]> hgetAll(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.hgetAll(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.hgetAll(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public List<byte[]> sort(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.sort(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.sort(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public List<byte[]> sort(byte[] key, SortingParams sortingParameters) {
-		redis =jedisPool.getResource(); 
-		return redis.sort(key, sortingParameters);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.sort(key, sortingParameters);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Boolean exists(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.exists(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.exists(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public String type(byte[] key) {
-		redis =jedisPool.getResource(); 
-		return redis.type(key);
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.type(key);
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Object set(String key, int expire, Object o) {
-		redis =jedisPool.getResource(); 
-		return set(key, expire, SerializableUtil.serialize(o));
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return set(key, expire, SerializableUtil.serialize(o));
+		} finally {
+			redis.close();
+		}
 	}
 
 	public Object getObject(String key) {
-		redis =jedisPool.getResource(); 
-		byte[] binary = getBinary(key);
-		if(binary ==null){
-			return null;
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			byte[] binary = getBinary(key);
+			if (binary == null) {
+				return null;
+			}
+			return SerializableUtil.deserialize(binary);
+		} finally {
+			redis.close();
 		}
-		return SerializableUtil.deserialize(binary);
 	}
 
-	public Object rpush(String key,String strings) {
-		redis =jedisPool.getResource(); 
-		return redis.rpush(key, strings);
+	public Object rpush(String key, String strings) {
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.rpush(key, strings);
+		} finally {
+			redis.close();
+		}
 	}
 
-	public List<String> lrange(String key,int start,int end) {
-		redis =jedisPool.getResource(); 
-		return redis.lrange(key, start, end);
+	public List<String> lrange(String key, int start, int end) {
+		Jedis redis = null;
+		try {
+			redis = redisPool.getResource();
+			return redis.lrange(key, start, end);
+		} finally {
+			redis.close();
+		}
 	}
 }
