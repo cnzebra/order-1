f8dbc7a1ca3770eb90c5552635142318b7496a7c
zhoujie <zhoujie@123feng.com>
2017-03-06 14:08:25 +0800

    修改运单解析兼容

--- src/main/java/com/mrwind/order/service/TaskService.java
+++ src/main/java/com/mrwind/order/service/TaskService.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
