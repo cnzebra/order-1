fcefcec41e242bcab301a3a55e03baa1bda3486e
zhoujie <zhoujie@123feng.com>
2017-04-02 08:49:12 +0800

    修改绑定运单查询接口2

--- src/main/java/com/mrwind/order/dao/ExpressDao.java
+++ src/main/java/com/mrwind/order/dao/ExpressDao.java
@@ -343,7 +343,9 @@ public class ExpressDao extends BaseDao {
 		Query query = new Query();
 		query.addCriteria(Criteria.where("shop._id").is(new ObjectId(shopId)));
 		query.addCriteria(Criteria.where("receiver.tel").is(tel));
-		query.addCriteria(Criteria.where("bindExpressNo").is(""));
+		Criteria criteria = new Criteria();
+		criteria.orOperator(Criteria.where("bindExpressNo").is(""),Criteria.where("bindExpressNo").is(null));
+		query.addCriteria(criteria);
 		return mongoTemplate.findOne(query, Express.class);
 	}
 }
